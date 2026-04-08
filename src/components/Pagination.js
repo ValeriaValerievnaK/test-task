11f9ef8acd5c4143b2f39679a46a5dec41c52f57
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2026-04-08 17:55:51 +0300

    исправление ошибок и предупреждений линтера

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,18 +1,23 @@
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useData } from './providers';
 
 export function Pagination() {
   const [pages, setPages] = useState([]);
   const { apiURL, info, activePage, setActivePage, setApiURL } = useData();
 
-  const pageClickHandler = (index) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    setActivePage(index);
-    setApiURL(pages[index]);
-  };
+  const pageClickHandler = useCallback(
+    (index) => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      setActivePage(index);
+      setApiURL(pages[index]);
+    },
+    [pages, setActivePage, setApiURL]
+  );
 
   useEffect(() => {
+    if (!apiURL) return;
+
     const createdPages = Array.from({ length: info.pages }, (_, i) => {
       const URLWithPage = new URL(apiURL);
 
@@ -22,7 +27,7 @@ export function Pagination() {
     });
 
     setPages(createdPages);
-  }, [info]);
+  }, [info.pages, apiURL]);
 
   if (pages.length <= 1) return null;
 
@@ -54,7 +59,9 @@ export function Pagination() {
           {activePage + 1 !== pages.length - 1 && (
             <>
               <Ellipsis>...</Ellipsis>
-              <Page onClick={() => pageClickHandler(pages.length)}>Last »</Page>
+              <Page onClick={() => pageClickHandler(pages.length - 1)}>
+                Last »
+              </Page>
             </>
           )}
         </>
@@ -79,14 +86,6 @@ const Page = styled.span`
   &:hover {
     color: #83bf46;
   }
-`;
-
-const Container = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  justify-items: center;
-  gap: 30px;
 `;
 
 const Ellipsis = styled(Page)`
